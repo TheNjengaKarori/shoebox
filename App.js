b9554b96159620359ed92cccee79990151df1fd1
Njengak11 <njengak11@gmail.com>
2022-03-09 19:01:30 +0300

    finished landing page

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { Button,  } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.shoe}>Shoe</Text>
+      <Text style={styles.box}>Box</Text>
       <StatusBar style="auto" />
+      <Button
+                title="LOG IN"
+                buttonStyle={{
+                  backgroundColor: 'black',
+                  borderWidth: 2,
+                  borderColor: 'white',
+                  borderRadius: 30,
+                }}
+                containerStyle={{
+                  width: 200,
+                  marginHorizontal: 50,
+                  marginVertical: 10,
+                }}
+                titleStyle={{ fontWeight: 'bold' }}
+              />
+      <Text>Sign Up</Text>
     </View>
   );
 }
@@ -17,4 +35,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  shoe:{
+    fontSize:80,
+    color:'red',
+    fontWeight:'bold'
+  },
+  box:{
+    fontSize:160,
+    color:'black',
+    fontWeight:'bold'
+  }
 });
